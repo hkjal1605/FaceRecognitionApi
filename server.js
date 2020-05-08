206917c1297507d073b4b28bdfe22b44ba71d304
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-05-08 14:56:25 +0100

    specfied the loaction of the database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controller/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-solid-82532',
     user : 'postgres',
     password : 1605,
     database : 'smartbrain'
